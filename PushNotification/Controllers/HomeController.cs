4fcc44de61a54ddc35575e232375fe1b9d35f6b9
saeed-balkani <saeedbalkani@gmail.com>
2021-08-25 15:42:47 +0200

    init1

---
+++ PushNotification/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PushNotification.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetNotificationContacts()
+        {
+            var notificationRegisterTime = Session["LastUpdated"] != null ? Convert.ToDateTime(Session["LastUpdated"]) : DateTime.Now;
+            NotificationComponent NC = new NotificationComponent();
+            var list = NC.GetContacts(notificationRegisterTime);
+            //update session here for get only new added contacts (notification)
+            Session["LastUpdate"] = DateTime.Now;
+            return new JsonResult { Data = list, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+	}
+}
---
+++ PushNotification/NotificationComponent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using Microsoft.AspNet.SignalR;
+using PushNotification;
+
+namespace PushNotification
+{
+    public class NotificationComponent
+    {
+        //Here we will add a function for register notification (will add sql dependency)
+        public void RegisterNotification(DateTime currentTime)
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["sqlConString"].ConnectionString;
+            string sqlCommand = @"SELECT [ContactID],[ContactName],[ContactNo] from [dbo].[Contact] where [AddedOn] > @AddedOn";
+            //you can notice here I have added table name like this [dbo].[Contacts] with [dbo], its mendatory when you use Sql Dependency
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand(sqlCommand, con);
+                cmd.Parameters.AddWithValue("@AddedOn", currentTime);
+                if (con.State != System.Data.ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                cmd.Notification = null;
+                SqlDependency sqlDep = new SqlDependency(cmd);
+                sqlDep.OnChange += sqlDep_OnChange;
+                //we must have to execute the command here
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    // nothing need to add here now
+                }
+            }
+        }
+
+        void sqlDep_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                SqlDependency sqlDep = sender as SqlDependency;
+                sqlDep.OnChange -= sqlDep_OnChange;
+
+                //from here we will send notification message to client
+                var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+                notificationHub.Clients.All.notify("added");
+
+                //re-register notification
+                RegisterNotification(DateTime.Now);
+
+            }
+        }
+
+        public List<contact> GetContacts(DateTime afterDate)
+        {
+            using (MyPushNotification dc = new MyPushNotification())
+            {
+                return dc.contacts.Where(a => a.addedOn > afterDate).OrderByDescending(a => a.addedOn).ToList();
+            }
+        }
+    }
+}
